ac2f66b1fb9ab8c8c37ab2f91d6acb737f219a65
Matias Guaymas <89048193+MatiasGuaymas@users.noreply.github.com>
2024-01-01 23:04:57 -0300

    TALLER 2023

--- 2. Objetos/TP_ProyectoAlumnos/src/tema1/Ej05.java
+++ 2. Objetos/TP_ProyectoAlumnos/src/tema1/Ej05.java
@@ -1,18 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/* 5- El dueño de un restaurante entrevista a cinco clientes y les pide que califiquen
+(con puntaje de 1 a 10) los siguientes aspectos: (0) Atención al cliente (1) Calidad
+de la comida (2) Precio (3) Ambiente.
+Escriba un programa que lea desde teclado las calificaciones de los cinco clientes
+para cada uno de los aspectos y almacene la información en una estructura. Luego
+imprima la calificación promedio obtenida por cada aspecto. */
+
 package tema1;
 
 import PaqueteLectura.Lector;
 import PaqueteLectura.GeneradorAleatorio;
 
-
-/**
- *
- * @author Matute
- */
 public class Ej05 {
     public static void main(String[] args) {
         GeneradorAleatorio.iniciar();
