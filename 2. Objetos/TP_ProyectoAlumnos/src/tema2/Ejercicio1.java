cc519f5a0f663c9a4a9b959674f65d77242aa635
Matias Guaymas <89048193+MatiasGuaymas@users.noreply.github.com>
2024-01-02 17:38:30 -0300

    TALLER 2023

--- 2. Objetos/TP_ProyectoAlumnos/src/tema2/Ejercicio1.java
+++ 2. Objetos/TP_ProyectoAlumnos/src/tema2/Ejercicio1.java
@@ -1,20 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/* 1- Se dispone de la clase Persona (en la carpeta tema2). Un objeto persona puede crearse
+sin valores iniciales o enviando en el mensaje de creación el nombre, DNI y edad (en ese
+orden). Un objeto persona responde a los siguientes mensajes:
+
+getNombre() retorna el nombre (String) de la persona
+getDNI() retorna el dni (int) de la persona
+getEdad() retorna la edad (int) de la persona
+setNombre(X) modifica el nombre de la persona al “String” pasado por parámetro (X)
+setDNI(X) modifica el DNI de la persona al “int” pasado por parámetro (X)
+setEdad(X) modifica la edad de la persona al “int” pasado por parámetro (X)
+toString() retorna un String que representa al objeto. Ej: “Mi nombre es Mauro,
+mi DNI es 11203737 y tengo 70 años”
+
+Realice un programa que cree un objeto persona con datos leídos desde teclado. Luego
+muestre en consola la representación de ese objeto en formato String. */
+
 package tema2;
 import PaqueteLectura.*;
 
-/**
- *
- * @author Matute
- */
 public class Ejercicio1 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Persona per = new Persona();
         System.out.println("Ingrese un nombre: ");
@@ -24,6 +27,8 @@ public class Ejercicio1 {
         System.out.println("Ingrese una edad: ");
         per.setEdad(Lector.leerInt());
         System.out.println(per.toString());
+        
+        Persona per2 = new Persona(Lector.leerString(), Lector.leerInt(), Lector.leerInt());
+        System.out.println(per2.toString());
     }
-    
 }
