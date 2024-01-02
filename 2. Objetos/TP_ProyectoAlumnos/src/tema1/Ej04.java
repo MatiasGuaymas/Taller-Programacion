a844c8e8f9252df16cb02caf1e06d9ae9804784d
Matias Guaymas <89048193+MatiasGuaymas@users.noreply.github.com>
2024-01-01 23:04:38 -0300

    TALLER 2023

--- 2. Objetos/TP_ProyectoAlumnos/src/tema1/Ej04.java
+++ 2. Objetos/TP_ProyectoAlumnos/src/tema1/Ej04.java
@@ -1,17 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/* 4- Un edificio de oficinas está conformado por 8 pisos (1..8) y 4 oficinas por piso
+(1..4). Realice un programa que permita informar la cantidad de personas que
+concurrieron a cada oficina de cada piso. Para esto, simule la llegada de personas al
+edificio de la siguiente manera: a cada persona se le pide el nro. de piso y nro. de
+oficina a la cual quiere concurrir. La llegada de personas finaliza al indicar un nro.
+de piso 9. Al finalizar la llegada de personas, informe lo pedido. */
+
 package tema1;
 
 import PaqueteLectura.Lector;
 import PaqueteLectura.GeneradorAleatorio;
 
-/**
- *
- * @author Matute
- */
 public class Ej04 {
     public static void main(String[] args) {
          GeneradorAleatorio.iniciar();
