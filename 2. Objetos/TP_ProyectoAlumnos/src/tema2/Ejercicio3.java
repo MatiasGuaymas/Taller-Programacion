b5a5264c164f4eb65ae9ed469fc48f684909dc30
Matias Guaymas <89048193+MatiasGuaymas@users.noreply.github.com>
2024-01-02 17:40:07 -0300

    TALLER 2023

--- 2. Objetos/TP_ProyectoAlumnos/src/tema2/Ejercicio3.java
+++ 2. Objetos/TP_ProyectoAlumnos/src/tema2/Ejercicio3.java
@@ -1,53 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/* 3- Se realizará un casting para un programa de TV. El casting durará a lo sumo 5 días y en
+cada día se entrevistarán a 8 personas en distinto turno.
+a) Simular el proceso de inscripción de personas al casting. A cada persona se le pide
+nombre, DNI y edad y se la debe asignar en un día y turno de la siguiente manera: las
+personas primero completan el primer día en turnos sucesivos, luego el segundo día y así
+siguiendo. La inscripción finaliza al llegar una persona con nombre “ZZZ” o al cubrirse los
+40 cupos de casting.
+Una vez finalizada la inscripción:
+b) Informar para cada día y turno asignado, el nombre de la persona a entrevistar.
+NOTA: utilizar la clase Persona. Pensar en la estructura de datos a utilizar. Para comparar
+Strings use el método equals. */
+
 package tema2;
 import PaqueteLectura.*;
-/**
- *
- * @author Matute
- */
-public class Ejercicio3 {
 
-    /**
-     * @param args the command line arguments
-     */
+public class Ejercicio3 {
     public static void main(String[] args) {
+        GeneradorAleatorio.iniciar();
         int dia = 5;
         int entrevistas = 8;
+        int entrevistados = 0;
+        int ultDia = -1;
+        int aux = 0;
+        
         Persona [][] entrevistado = new Persona[dia][entrevistas];
-        GeneradorAleatorio.iniciar();
-        int i=0; int j= 0;
-        String nom = GeneradorAleatorio.generarString(5);
+        int [] vecDiml = new int[dia];
         
-        //Empiezo dia 0
-        while(!nom.equals("ZZZ")&&(i<dia)) {
-            j=0; //Empieza desde la entrevista 0
-            while(!nom.equals("ZZZ")&&(j<entrevistas)) {
-                entrevistado[i][j] = new Persona();
-                entrevistado[i][j].setNombre(nom);
-                nom = GeneradorAleatorio.generarString(5);//HAGO ESTO PORQUE SI NO TODOS LOS STRINGS DEL DIA 0 SON LOS MISMOS
-                entrevistado[i][j].setDNI(GeneradorAleatorio.generarInt(30));
-                entrevistado[i][j].setEdad(GeneradorAleatorio.generarInt(100));
-                j++;
-            }
-            i++; // Paso a otro dia
-            nom = GeneradorAleatorio.generarString(5);
+        int i;
+        for (i = 0; i < 5; i++) {
+            vecDiml[i] = 0;
         }
-        System.out.println(i + " " + j);
         
-        int a= 0; int b = 0;
+        String nom = GeneradorAleatorio.generarString(3);
         
-        while(a < i ) {
-            while(b < j && entrevistado[a][b] != null) {
-                System.out.println(entrevistado[a][b].toString());
-                b++; //Paso al turno siguiente
+        while(!nom.equals("ZZZ")&&(entrevistados < 40)) {
+            entrevistado[aux][vecDiml[aux]] = new Persona(nom, GeneradorAleatorio.generarInt(30), GeneradorAleatorio.generarInt(100));
+            vecDiml[aux]++; // Aumento en 1 la cantidad de anotados para el dia aux
+            ultDia = aux;
+            if(vecDiml[aux] == 8) 
+                aux++; // Paso a otro dia
+            nom = GeneradorAleatorio.generarString(3);
+            entrevistados++;
+        }
+        
+        for(int a = 0; a <= ultDia; a++) {
+            for(int b = 0; b < vecDiml[a]; b++) {
+                System.out.println("Dia " + a + " entrevista " + b + " es de la persona: " + entrevistado[a][b].getNombre());
             }
-            a++; //Paso al dia siguiente
-            b=0; //Reinicio para la siguiente fila
         }
     }
-    
 }
