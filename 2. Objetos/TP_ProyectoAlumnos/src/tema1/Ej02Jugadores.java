f31e3861ad4ee0918849159efa30f18dfaba8ff7
Matias Guaymas <89048193+MatiasGuaymas@users.noreply.github.com>
2024-01-01 23:04:19 -0300

    TALLER 2023

--- 2. Objetos/TP_ProyectoAlumnos/src/tema1/Ej02Jugadores.java
+++ 2. Objetos/TP_ProyectoAlumnos/src/tema1/Ej02Jugadores.java
@@ -1,3 +1,8 @@
+/* 2- Escriba un programa que lea las alturas de los 15 jugadores de un equipo de
+básquet y las almacene en un vector. Luego informe:
+- la altura promedio
+- la cantidad de jugadores con altura por encima del promedio
+NOTA: Dispone de un esqueleto para este programa en Ej02Jugadores.java */
 
 package tema1;
 
@@ -5,19 +10,18 @@ package tema1;
 import PaqueteLectura.Lector;
 
 public class Ej02Jugadores {
-
   
     public static void main(String[] args) {
-        int DIMF= 5;
+        int DIMF= 15;
         //Paso 2: Declarar la variable vector de double 
         double[] jugadores;
         //Paso 3: Crear el vector para 15 double 
         jugadores = new double[DIMF];
         //Paso 4: Declarar indice y variables auxiliares a usar
         int i;
-         double suma = 0;
+        double suma = 0;
         //Paso 6: Ingresar 15 numeros (altura), cargarlos en el vector, ir calculando la suma de alturas
-        for(i=0; i<= 4; i++){
+        for(i=0; i<= 14; i++){
             System.out.print("Ingrese una altura: ");
             jugadores[i] = Lector.leerDouble();
             suma += jugadores [i];
@@ -25,10 +29,10 @@ public class Ej02Jugadores {
         
         //Paso 7: Calcular el promedio de alturas, informarlo
         suma=(suma/DIMF);
-         System.out.println("El promedio de las alturas de los jugadores es de: " + suma);
+        System.out.println("El promedio de las alturas de los jugadores es de: " + suma);
         //Paso 8: Recorrer el vector calculando lo pedido (cant. alturas que están por encima del promedio)
         int cant= 0;
-        for(i=0; i<= 4; i++) 
+        for(i=0; i<= 14; i++) 
             if(jugadores[i] > suma)
                 cant++;
         //Paso 9: Informar la cantidad.
