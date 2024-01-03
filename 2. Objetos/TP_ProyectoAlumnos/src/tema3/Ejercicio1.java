46976b1efa0ba49956964ae75c76c3e53afb6620
Matias Guaymas <89048193+MatiasGuaymas@users.noreply.github.com>
2024-01-03 12:49:15 -0300

    TALLER 2023

--- 2. Objetos/TP_ProyectoAlumnos/src/tema3/Ejercicio1.java
+++ 2. Objetos/TP_ProyectoAlumnos/src/tema3/Ejercicio1.java
@@ -1,26 +1,23 @@
+/* 1-A- Definir una clase para representar triángulos. Un triángulo se caracteriza por el
+tamaño de sus 3 lados (double), el color de relleno (String) y el color de línea (String).
+Provea un constructor que reciba todos los datos necesarios para iniciar el objeto.
+Provea métodos para:
+- Devolver/modificar el valor de cada uno de sus atributos (métodos get y set)
+- Calcular el perímetro y devolverlo (método calcularPerimetro)
+- Calcular el área y devolverla (método calcularArea)
+B- Realizar un programa que instancie un triángulo, le cargue información leída desde
+teclado e informe en consola el perímetro y el área.
+NOTA: Calcular el área con la fórmula Área = √(s(s − a)(s − b)(s − c)) , donde a, b y c son
+los lados y s = (a+b+c)/2 . La función raíz cuadrada es Math.sqrt(#) */
+
 package tema3;
 
 import PaqueteLectura.*;
 
 public class Ejercicio1 {
-
     public static void main(String[] args) {
-        Triangulo triangulo1 = new Triangulo();
-        String color;
-        GeneradorAleatorio.iniciar();
-        triangulo1.setLado1(GeneradorAleatorio.generarDouble(50));
-        triangulo1.setLado2(GeneradorAleatorio.generarDouble(50));
-        triangulo1.setLado3(GeneradorAleatorio.generarDouble(50));
-        System.out.println("Ingrese color de relleno");
-        color = Lector.leerString();
-        triangulo1.setColorRelleno(color);
-        System.out.println("Ingrese color de borde");
-        color = Lector.leerString();
-        triangulo1.setColorLinea(color);
-        
+        Triangulo triangulo1 = new Triangulo(Lector.leerDouble(), Lector.leerDouble(), Lector.leerDouble(), Lector.leerString(), Lector.leerString());
         System.out.println(triangulo1.calcularPerimetro());
-        
         System.out.println(triangulo1.calcularArea());
     }
-    
 }
