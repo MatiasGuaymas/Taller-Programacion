0ebe1a1da63a52c791ca890a0d173b9f127bdc61
Matias Guaymas <89048193+MatiasGuaymas@users.noreply.github.com>
2024-01-03 12:50:33 -0300

    TALLER 2023

--- 2. Objetos/TP_ProyectoAlumnos/src/tema3/Ejercicio4.java
+++ 2. Objetos/TP_ProyectoAlumnos/src/tema3/Ejercicio4.java
@@ -1,74 +1,42 @@
+/* 4-A- Un hotel posee N habitaciones. De cada habitación conoce costo por noche, si está
+ocupada y, en caso de estarlo, guarda el cliente que la reservó (nombre, DNI y edad).
+(i) Genere las clases necesarias. Para cada una provea métodos getters/setters adecuados.
+(ii) Implemente los constructores necesarios para iniciar: los clientes a partir de nombre,
+DNI, edad; el hotel para N habitaciones, cada una desocupada y con costo aleatorio e/
+2000 y 8000.
+(iii) Implemente en las clases que corresponda todos los métodos necesarios para:
+- Ingresar un cliente C en la habitación número X. Asuma que X es válido (es decir, está
+en el rango 1..N) y que la habitación está libre.
+- Aumentar el precio de todas las habitaciones en un monto recibido.
+- Obtener la representación String del hotel, siguiendo el formato:
+{Habitación 1: costo, libre u ocupada, información del cliente si está ocupada}
+…
+{Habitación N: costo, libre u ocupada, información del cliente si está ocupada}
+B- Realice un programa que instancie un hotel, ingrese clientes en distintas habitaciones,
+muestre el hotel, aumente el precio de las habitaciones y vuelva a mostrar el hotel.
+NOTAS: Reúse la clase Persona. Para cada método solicitado piense a qué clase debe
+delegar la responsabilidad de la operación. */
+
 package tema3;
 
 import PaqueteLectura.*;
 
-/**
- *
- * @author Matute
- */
-
-//Deberia usar la clase Persona, pero bueno tengo Cliente, y tampoco tengo tiempo (:
+//Deberia usar la clase Persona, pero cree una clase Cliente
 
 public class Ejercicio4 {
 
     public static void main(String[] args) {
         
-        Hotel hotel1 = new Hotel(20);
-        Cliente cliente1;
-        String nom;
-        int edad, dni,nro;
+        Hotel hotel1 = new Hotel(15);
         int i;
-        
-        for(i=0;i<20;i++){
-            hotel1.getHabitaciones()[i]= new Habitacion();
+        int numHab = 1;
+        for (i=0; i < 8; i++) {
+            Cliente c = new Cliente(GeneradorAleatorio.generarString(5), GeneradorAleatorio.generarInt(50)+100, GeneradorAleatorio.generarInt(100));
+            hotel1.agregarCliente(c, numHab);
+            numHab += 2;
         }
-        
-        int dl = 0;
-        //System.out.println("Ingrese nro habitacion");
-        //nro = Lector.leerInt();
-        
-        nro = GeneradorAleatorio.generarInt(21);
-        
-        while((nro != -1)&&(dl < 20)){
-        //System.out.println("Ingrese dato Nombre");
-        //nom = Lector.leerString();
-        nom = GeneradorAleatorio.generarString(10);
-        //System.out.println("Ingrese dato Edad");
-        edad = GeneradorAleatorio.generarInt(20);
-        //edad = Lector.leerInt();
-        //System.out.println("Ingrese dato Dni");
-        dni = GeneradorAleatorio.generarInt(20);
-        //dni = Lector.leerInt();
-        cliente1 = new Cliente(nom, dni, edad);
-        if(!hotel1.getHabitaciones()[nro].isOcupada()){
-            hotel1.getHabitaciones()[nro].setCliente(cliente1);
-            hotel1.getHabitaciones()[nro].setOcupada(true);
-        }
-        else
-           System.out.println("OCUPADA");
-        System.out.println("Ingrese nro habitacion");
-        dl++;
-        nro = Lector.leerInt();
-        }
-        
-        for(i=0;i<20;i++){
-            System.out.println("la habitacion "+i+" esta ocupada: "+hotel1.getHabitaciones()[i].isOcupada()+ " su precio era de : " + hotel1.getHabitaciones()[i].getCosto());
-            if(hotel1.getHabitaciones()[i].isOcupada()){
-                System.out.println("por: "+hotel1.getHabitaciones()[i].getCliente().getNombre()+ " su precio era de : " + hotel1.getHabitaciones()[i].getCosto()) ;
-            }
-        }
-        
-        double suma = 100.1;
-        
-        System.out.println("Hay un aumento de 100.1");
-        
-        for(i=0;i<20;i++){
-            System.out.println("la habitacion "+i+" esta ocupada: "+hotel1.getHabitaciones()[i].isOcupada()+ " su precio era de : " + hotel1.getHabitaciones()[i].aumentarPrecio(suma));
-            if(hotel1.getHabitaciones()[i].isOcupada()){
-                System.out.println("por: "+hotel1.getHabitaciones()[i].getCliente().getNombre()+ " su precio era de : " + hotel1.getHabitaciones()[i].aumentarPrecio(suma)) ;
-            }
-        }
-        
+        System.out.println(hotel1.toString());
+        hotel1.aumentarPrecios(100.10);
+        System.out.println(hotel1.toString());
     }
-    
 }
