30f5cda771e2b643111b807e3fdea05ff271b265
Matias Guaymas <89048193+MatiasGuaymas@users.noreply.github.com>
2024-01-03 12:49:34 -0300

    TALLER 2023

--- 2. Objetos/TP_ProyectoAlumnos/src/tema3/Ejercicio2.java
+++ 2. Objetos/TP_ProyectoAlumnos/src/tema3/Ejercicio2.java
@@ -1,8 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/* 2-A- Modifique la clase Libro.java (carpeta tema3) para ahora considerar que el primer
+autor es un objeto instancia de la clase Autor.
+Implemente la clase Autor, sabiendo que se caracterizan por nombre, biografía y origen
+y que deben permitir devolver/modificar el valor de sus atributos y devolver una
+representación String formada por nombre, biografía y origen.
+Luego realice las modificaciones necesarias en la clase Libro.
+B- Modifique el programa Demo01Constructores (carpeta tema3) para instanciar los
+libros con su autor, considerando las modificaciones realizadas. Luego, a partir de uno de
+los libros instanciados, obtenga e imprima la representación del autor de ese libro. */
+
 package tema3;
 
 import PaqueteLectura.*;
@@ -11,16 +16,10 @@ public class Ejercicio2 {
     
     public static void main(String[] args) {
         
-       Autor unAutor = new Autor();
-       Autor unAutor2 = new Autor();
-       unAutor.setBiografia(GeneradorAleatorio.generarString(10));
-       unAutor.setOrigen(GeneradorAleatorio.generarString(10));
-       unAutor.setNombre(GeneradorAleatorio.generarString(10));
-       unAutor2.setBiografia(GeneradorAleatorio.generarString(10));
-       unAutor2.setOrigen(GeneradorAleatorio.generarString(10));
-       unAutor2.setNombre(GeneradorAleatorio.generarString(10));
+       Autor unAutor = new Autor(GeneradorAleatorio.generarString(8), GeneradorAleatorio.generarString(10), GeneradorAleatorio.generarString(5));
+       Autor unAutor2 = new Autor(GeneradorAleatorio.generarString(8), GeneradorAleatorio.generarString(10), GeneradorAleatorio.generarString(5));
        
-       Libro libro1= new  Libro( "Java: A Beginner's Guide", "Mcgraw-Hill", 2014,unAutor, "978-0071809252", 21.72);
+       Libro libro1= new  Libro( "Java: A Beginner's Guide", "Mcgraw-Hill", 2014, unAutor, "978-0071809252", 21.72);
        Libro libro2= new Libro("Learning Java by Building Android Games",  
                               "CreateSpace Independent Publishing", unAutor2, "978-1512108347");
         System.out.println(libro1.toString());
