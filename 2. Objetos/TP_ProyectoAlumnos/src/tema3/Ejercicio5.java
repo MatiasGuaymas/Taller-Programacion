f90da34548e53356fcb2f7908c8280c59ab6c97a
Matias Guaymas <89048193+MatiasGuaymas@users.noreply.github.com>
2024-01-03 12:50:49 -0300

    TALLER 2023

--- 2. Objetos/TP_ProyectoAlumnos/src/tema3/Ejercicio5.java
+++ 2. Objetos/TP_ProyectoAlumnos/src/tema3/Ejercicio5.java
@@ -1,11 +1,18 @@
+/* 5-A- Definir una clase para representar círculos. Los círculos se caracterizan por su radio
+(double), el color de relleno (String) y el color de línea (String).
+Provea un constructor que reciba todos los datos necesarios para iniciar el objeto.
+Provea métodos para:
+- Devolver/modificar el valor de cada uno de sus atributos (métodos get y set)
+- Calcular el perímetro y devolverlo (método calcularPerimetro)
+- Calcular el área y devolverla (método calcularArea)
+B- Realizar un programa que instancie un círculo, le cargue información leída de teclado e
+informe en consola el perímetro y el área.
+NOTA: la constante PI es Math.PI */
+
 package tema3;
 
 import PaqueteLectura.*;
 
-/**
- *
- * @author Matute
- */
 public class Ejercicio5 {
     
     public static void main(String[] args) {
